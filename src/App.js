b436b52ceab7c9b9d9f6d302402f63e470305527
Dave <alexruedarivas@outlook.com>
2020-10-13 18:15:35 -0500

    link improve

--- src/App.js
+++ src/App.js
@@ -7,12 +7,8 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/Noticia:id">
-          <Noticia />
-        </Route>
+        <Route exact path="/" component={Home} />
+        <Route path="/Noticia/:id" component={Noticia} />
       </Switch>
     </Router>
   );
